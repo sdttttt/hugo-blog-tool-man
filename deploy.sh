d043b5da4b41e584c1749c9ab038a536d71b2254
Psnail <42728902+sdttttt@users.noreply.github.com>
2020-10-29 17:13:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -124,7 +124,7 @@ function syncSourceCode {
 function generateSite {
 
     successLog "HugoGenerator" "🚚 Hugo Building..."
-    hugo
+    hugo --enableGitInfo --print-mem --minify  -v
 
     if [ -d "./public" ]; then
         mv ./public ./docs
